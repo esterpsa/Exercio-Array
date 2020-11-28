19530fae8bcabb68a39a2dbddf603bd0079e3f4f
Ester <ester.albuquerque500@gmail.com>
2020-11-28 09:16:59 -0300

    Array Finalizado

--- Program.cs
+++ Program.cs
@@ -6,58 +6,57 @@ namespace Função_A
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Array");
-            Console.WriteLine("CADASTRO: KATA POR EQUIPE");
-            Console.WriteLine("---------------");
+            // Console.WriteLine("Array");
+            // Console.WriteLine("CADASTRO: KATA POR EQUIPE");
+            // Console.WriteLine("---------------");
 
-            string[] nomes = new string [3];
-            int [] idade = new int [3];
-            string [] estilo = new string [3];
-            string [] academia = new string [3];
-            string [] graduacao = new string [3];
+            // string[] nomes = new string [3];
+            // int [] idade = new int [3];
+            // string [] estilo = new string [3];
+            // string [] academia = new string [3];
+            // string [] graduacao = new string [3];
 
-            for(var contador = 0; contador <3;contador++)
-            {
-                Console.WriteLine($"Digite o {contador+1}º nome:");
-                nomes [contador] =Console.ReadLine();
-                Console.WriteLine("Digite a idade: ");
-                idade [contador] = int.Parse(Console.ReadLine());
-                Console.WriteLine("Digite o estilo: ");
-                estilo [contador] = Console.ReadLine();
-                Console.WriteLine("Digite o nome da academia: ");
-                academia [contador] = Console.ReadLine();
-                Console.WriteLine("Digite a sua graduação: ");
-                graduacao [contador] = Console.ReadLine();
-            }
+            // for(var contador = 0; contador <3;contador++)
+            // {
+            //     Console.WriteLine($"Digite o {contador+1}º nome:");
+            //     nomes [contador] =Console.ReadLine();
+            //     Console.WriteLine("Digite a idade: ");
+            //     idade [contador] = int.Parse(Console.ReadLine());
+            //     Console.WriteLine("Digite o estilo: ");
+            //     estilo [contador] = Console.ReadLine();
+            //     Console.WriteLine("Digite o nome da academia: ");
+            //     academia [contador] = Console.ReadLine();
+            //     Console.WriteLine("Digite a sua graduação: ");
+            //     graduacao [contador] = Console.ReadLine();
+            // }
 
-            Console.WriteLine("Equipe cadastrada");
-            Console.WriteLine("-------------------");
+            // Console.WriteLine("Equipe cadastrada");
+            // Console.WriteLine("-------------------");
 
-            for(var contador = 0; contador <3; contador++)
-            {
-                Console.WriteLine("nomes: "+nomes[contador]);
-                Console.WriteLine("idade: "+idade[contador]);
-                Console.WriteLine("estilo: "+estilo[contador]);
-                Console.WriteLine("academia: "+academia[contador]);
-                Console.WriteLine("graduacao: "+graduacao[contador]);
-            }
+            // for(var contador = 0; contador <3; contador++)
+            // {
+            //     Console.WriteLine("nomes: "+nomes[contador]);
+            //     Console.WriteLine("idade: "+idade[contador]);
+            //     Console.WriteLine("estilo: "+estilo[contador]);
+            //     Console.WriteLine("academia: "+academia[contador]);
+            //     Console.WriteLine("graduacao: "+graduacao[contador]);
+            // }
        
 
           Console.WriteLine("Está precisando de luvas? ");
           string material = Console.ReadLine();
 
+             string [] cores = {"Ao, Aka",};
+             string [] tamanhos = {"G,GG, M, P"};
+
           if( material  == "não"){
               Console.WriteLine("Boa sorte no campeonato! oss");
           }else{
               Console.WriteLine("Conheça a loja da federação");
-          }
-        
-         string [] cores = {"Ao, Aka",};
-         string [] tamanhos = {"G,GG, M, P"};
-        
-        Console.WriteLine("Qual cor você precisa?");
+              Console.WriteLine("---------------------");
+              Console.WriteLine("Qual cor você precisa?");
 
-             for(int contador = 0; contador < cores.Length; contador++)
+                for(int contador = 0; contador < cores.Length; contador++)
             {
                 Console.WriteLine("Selecione uma opção: "+cores[contador]);
                  string respostacores = Console.ReadLine();
@@ -72,12 +71,9 @@ namespace Função_A
 
         Console.WriteLine("Prontinho! Pedido feito, aguarde para o pagamento. ");
         Console.WriteLine("Boa sorte! Oss");
-        
-    
-
-
-
 
         }
     }
+}
+
 }
